f4f096bc90d1158816d5f673a5cea50405d2f079
pcskl <pawel.czanasz@gmail.com>
2016-05-12 13:59:56 +0200

    args

--- NBPCurrencyExchangeRates/src/main/java/pl/parser/nbp/MainClass.java
+++ NBPCurrencyExchangeRates/src/main/java/pl/parser/nbp/MainClass.java
@@ -12,9 +12,9 @@ public class MainClass {
 	
 	public static void main(String[] args) throws ParseException {
 		
-		/*String currencySymbol = args[0];
+		String currencySymbol = args[0];
 		String startDate = args[1];
-		String endDate = args[2];*/
+		String endDate = args[2];
 		
 		Parser parser = Parser.getInstance();
 		Utility utility = Utility.getInstance();
@@ -22,14 +22,14 @@ public class MainClass {
 		List<String> listWithAdresses = null;
 		List<Currency> currencies = null;
 		try {
-			listWithAdresses = utility.getAdresses("2013-01-28", "2013-01-31");
+			listWithAdresses = utility.getAdresses(startDate, endDate);
 			utility.downloadXmlFilesFromParticularDays(listWithAdresses);
-			currencies = parser.parseDataFromXmlFiles(listWithAdresses, "eur");
+			currencies = parser.parseDataFromXmlFiles(listWithAdresses, currencySymbol);
 			System.out.println(statistics.averageBuyRate(currencies));
 			System.out.println(statistics.standardDeviationSellRate(currencies));
 		} 
 		catch (ParserConfigurationException | SAXException | IOException e) {
-			//e.printStackTrace();
+			e.printStackTrace();
 		}
 		finally {
 			utility.deleteXmlFiles(listWithAdresses);
